dfd8af71f5e7e5db23d633929f072ad14623d974
Haynan Penha <haynan.mesquita@gmail.com>
2025-04-24 11:05:43 -0300

    feat: Add users show and delete functions with validation

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -55,5 +56,29 @@ class UserController extends Controller
         return redirect()
             ->route('users.index')
             ->with('success', 'Usuário atualizado com sucesso.');
+    }
+
+    public function show(string $id)
+    {
+        if (!$user = User::find($id)) {
+            return back()->with('message', 'Usuário não encontrado.');
+        }
+        return view('admin.users.show', compact('user'));
+    }
+
+    public function destroy(string $id)
+    {
+        if (!$user = User::find($id)) {
+            return back()->with('message', 'Usuário não encontrado.');
+        }
+
+        if (Auth::user()->id == $user->id) {
+            return back()->with('message', 'Você não pode excluir sua própria conta.');
+        }
+        $user->delete();
+
+        return redirect()
+            ->route('users.index')
+            ->with('success', 'Usuário excluído com sucesso.');
     }
 }
