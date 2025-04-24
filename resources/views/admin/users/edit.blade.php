25883837bb1f9575ef14407eee914af1c582e889
Haynan Penha <haynan.mesquita@gmail.com>
2025-04-24 10:01:55 -0300

    feat: Refactor user create and edit forms to use partials for better maintainability

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,15 +4,10 @@
 
 @section('content')
 <h1>Editar Usuário {{ $user->name }}</h1>
-<x-alert />
 
 <form action="{{ route('users.update', $user->id) }}" method="POST">
-    @csrf()
     @method('PUT')
-    <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
-    <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
+    @include('admin.users.partials.form')
 </form>
 
 @endsection
